8d139c5d0e6b9edd7d7bf91845bc12e8d3ddd11f
nedelva <nedelva>
2023-09-22 04:22:55 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+## Micronaut 4.1.1 Documentation
+
+- [User Guide](https://docs.micronaut.io/4.1.1/guide/index.html)
+- [API Reference](https://docs.micronaut.io/4.1.1/api/index.html)
+- [Configuration Reference](https://docs.micronaut.io/4.1.1/guide/configurationreference.html)
+- [Micronaut Guides](https://guides.micronaut.io/index.html)
+---
+
+## Handler
+
+Handler: io.micronaut.function.aws.proxy.payload1.ApiGatewayProxyRequestEventFunction
+
+[AWS Lambda Handler](https://docs.aws.amazon.com/lambda/latest/dg/java-handler.html)
+
+- [Micronaut Maven Plugin documentation](https://micronaut-projects.github.io/micronaut-maven-plugin/latest/)
+## Feature http-client documentation
+
+- [Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#nettyHttpClient)
+
+
+## Feature aws-lambda-events-serde documentation
+
+- [Micronaut AWS Lambda Events Serde documentation](https://micronaut-projects.github.io/micronaut-aws/snapshot/guide/#eventsLambdaSerde)
+
+- [https://github.com/aws/aws-lambda-java-libs/tree/main/aws-lambda-java-events](https://github.com/aws/aws-lambda-java-libs/tree/main/aws-lambda-java-events)
+
+
+## Feature maven-enforcer-plugin documentation
+
+- [https://maven.apache.org/enforcer/maven-enforcer-plugin/](https://maven.apache.org/enforcer/maven-enforcer-plugin/)
+
+
+## Feature serialization-jackson documentation
+
+- [Micronaut Serialization Jackson Core documentation](https://micronaut-projects.github.io/micronaut-serialization/latest/guide/)
+
+
+## Feature micronaut-aot documentation
+
+- [Micronaut AOT documentation](https://micronaut-projects.github.io/micronaut-aot/latest/guide/)
+
+
+## Feature aws-lambda documentation
+
+- [Micronaut AWS Lambda Function documentation](https://micronaut-projects.github.io/micronaut-aws/latest/guide/index.html#lambda)
+
+
+## Feature snapstart documentation
+
+- [https://docs.aws.amazon.com/lambda/latest/dg/snapstart.html](https://docs.aws.amazon.com/lambda/latest/dg/snapstart.html)
+
+
+## Feature aws-lambda-custom-runtime documentation
+
+- [Micronaut Custom AWS Lambda runtime documentation](https://micronaut-projects.github.io/micronaut-aws/latest/guide/index.html#lambdaCustomRuntimes)
+
+- [https://docs.aws.amazon.com/lambda/latest/dg/runtimes-custom.html](https://docs.aws.amazon.com/lambda/latest/dg/runtimes-custom.html)
+
+
---
+++ src/test/java/com/example/HomeControllerTest.java
@@ -0,0 +1,97 @@
+package com.example;
+import com.amazonaws.services.lambda.runtime.ClientContext;
+import com.amazonaws.services.lambda.runtime.CognitoIdentity;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import io.micronaut.function.aws.proxy.payload1.ApiGatewayProxyRequestEventFunction;
+import io.micronaut.http.HttpMethod;
+import io.micronaut.http.HttpStatus;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HomeControllerTest {
+    private static ApiGatewayProxyRequestEventFunction handler;
+
+    @BeforeAll
+    public static void setupSpec() {
+        handler = new ApiGatewayProxyRequestEventFunction();
+    }
+    @AfterAll
+    public static void cleanupSpec() {
+        handler.getApplicationContext().close();
+    }
+
+    @Test
+    void testHandler() {
+        APIGatewayProxyRequestEvent request = new APIGatewayProxyRequestEvent();
+        request.setPath("/");
+        request.setHttpMethod(HttpMethod.GET.toString());
+        APIGatewayProxyResponseEvent response = handler.handleRequest(request, lambdaContext);
+        assertEquals(HttpStatus.OK.getCode(), response.getStatusCode());
+        assertEquals("{\"message\":\"Hello World\"}",  response.getBody());
+    }
+
+   static final Context lambdaContext = new Context() {
+
+        @Override
+        public String getAwsRequestId() {
+            return null;
+        }
+
+        @Override
+        public String getLogGroupName() {
+            return null;
+        }
+
+        @Override
+        public String getLogStreamName() {
+            return null;
+        }
+
+        @Override
+        public String getFunctionName() {
+            return null;
+        }
+
+        @Override
+        public String getFunctionVersion() {
+            return null;
+        }
+
+        @Override
+        public String getInvokedFunctionArn() {
+            return null;
+        }
+
+        @Override
+        public CognitoIdentity getIdentity() {
+            return null;
+        }
+
+        @Override
+        public ClientContext getClientContext() {
+            return null;
+        }
+
+        @Override
+        public int getRemainingTimeInMillis() {
+            return 0;
+        }
+
+        @Override
+        public int getMemoryLimitInMB() {
+            return 0;
+        }
+
+        @Override
+        public LambdaLogger getLogger() {
+            return null;
+        }
+    };
+}
